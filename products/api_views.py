0a9c2839bebedf9cead9406ae3a4dbaf8a9339cd
Victor Volkov <vektorn1212@gmail.com>
2024-03-06 10:54:14 +0200

    added order view and updated stripe session view

--- products/api_views.py
+++ products/api_views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 
-from products.models import Item
+from products.models import Item, Order
 
 
 class ItemDetailIView(APIView):
@@ -19,6 +19,20 @@ class ItemDetailIView(APIView):
         return render(request, 'item_detail.html', {'object': item})
 
 
+class OrderDetailIView(APIView):
+    """
+    Retrieves html page order from the database.
+    """
+
+    @staticmethod
+    def get(request, *args, **kwargs):
+        order = get_object_or_404(Order, pk=kwargs.get('pk'))
+        order.calculate_total_price()
+        order.name = f'Order {order.pk}'
+        order.save()
+        return render(request, 'order_detail.html', {'object': order})
+
+
 class StripeSessionDetail(APIView):
     """
     Retrieves stripe's session id.
@@ -27,19 +41,23 @@ class StripeSessionDetail(APIView):
 
     @staticmethod
     def get(request, *args, **kwargs):
-        item = get_object_or_404(Item, pk=kwargs.get('pk'))
-
-        item_name = item.name
-        item_price = item.price * 100
+        if 'order' in request.path:
+            order = get_object_or_404(Order, pk=kwargs.get('pk'))
+            stripe_name = order.name
+            stripe_price = order.total_price * 100
+        else:
+            item = get_object_or_404(Item, pk=kwargs.get('pk'))
+            stripe_name = item.name
+            stripe_price = item.price * 100
 
         session = stripe.checkout.Session.create(
             line_items=[{
                 'price_data': {
                     'currency': 'usd',
                     'product_data': {
-                        'name': item_name,
+                        'name': stripe_name,
                     },
-                    "unit_amount_decimal": item_price
+                    "unit_amount_decimal": stripe_price
                 },
                 'quantity': 1,
             }],

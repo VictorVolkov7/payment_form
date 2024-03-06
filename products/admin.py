96a7cc7d9bfcdf745afb3555cf7952cabfea42cd
Victor Volkov <vektorn1212@gmail.com>
2024-03-06 10:53:41 +0200

    register order model in django admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from products.models import Item
+from products.models import Item, Order
 
 
 @admin.register(Item)
@@ -8,3 +8,9 @@ class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'price',)
     search_fields = ('name',)
 
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    readonly_fields = ('name', 'total_price',)
